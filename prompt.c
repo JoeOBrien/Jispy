2b6b81c3bdca7420179b83362ffd6521ce36e7fd
Joe O'Brien <joe@joeobrien.me>
2014-04-05 21:01:56 +0100

    Added editline support to jispy

--- prompt.c
+++ prompt.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include <editline/history.h>
 
-static char input[2048];
 
 int main(int argc, char** argv) {
 	puts("Jispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 	while(1){
-		fputs("jispy> ", stdout);
-		fgets(input, 2048, stdin);
-		printf("No you're a %s", input);
+		char* input = readline("jispy> ");
+		add_history(input);
+		printf("No you're a %s\n", input);
+		free(input);			
 	}
 	return 0;
 }
