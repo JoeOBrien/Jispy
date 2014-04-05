9379802d2b12599746142201e5a2e803097ab8cc
Joe O'Brien <joe@joeobrien.me>
2014-04-05 23:50:44 +0100

    Added windows support sort of. lol windows is poison.

--- prompt.c
+++ prompt.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+fhar* readline(char* prompt) {
+	fputs("jispy> ", stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1)
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+
 #include <editline/readline.h>
 #include <editline/history.h>
 
+#endif
 
 int main(int argc, char** argv) {
 	puts("Jispy Version 0.0.0.0.1");
